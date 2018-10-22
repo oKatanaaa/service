bece4e0bf36566f78226cfdf2827cdc40cfa67db
oKatanaaa <whitemarsstudios@gmail.com>
2018-10-22 16:07:59 +0400

    DVA is corrected

--- src/cluster_worker/algorithms/distanceVariationAlgorithm/DVA.py
+++ src/cluster_worker/algorithms/distanceVariationAlgorithm/DVA.py
@@ -35,12 +35,6 @@ class DVA(Algorithm):
         # Add point and its neighbors to graph
         self.graph.add_point(point, set_neighbors)
 
-        # Get list of points which connections is needed to correct
-        list_points_to_correct = list(map_neighbors)
-
-        # Correct graph structure
-        self.__correct_graph_structure(list_points_to_correct)
-
     def delete_point(self, point: Point):
         # Get list of points which connections is needed to correct
         list_neighbors = self.graph.get_neighbours_list(point)
@@ -101,18 +95,5 @@ class DVA(Algorithm):
             list_tuples_current_neighbors = self.graph.get_neighbours_list(point)
 
             # Get distance to nearest neighbor
-            num_nearest_distance = list_tuples_current_neighbors[0][0]
-
-            # Create a list of new neighbors
-            filter_tuples = filter(lambda x: x[0] <= num_nearest_distance * self.dist_multiplier,
-                                   list_tuples_current_neighbors)
-            list_new_neighbors = list(filter_tuples)
-
-            # Check if we need don't need to correct current connections
-            if len(list_new_neighbors) == len(list_tuples_current_neighbors):
-                continue
-
-            # Get only points and set them as new neighbors
-            map_new_neighbors = map(lambda x: x[1], list_new_neighbors)
-            set_new_neighbors = set(map_new_neighbors)
-            self.graph.set_neighbours(point, set_new_neighbors)
+            if len(list_tuples_current_neighbors) == 0 or list_tuples_current_neighbors is None:
+                self.add_point(point)
